48356387330944639383adf71ecfa41b89c085ec
Kaspar <kasparsu@gmail.com>
2019-11-01 08:31:41 +0200

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(el, key) in list" :key="key">{{el}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+    mounted(){
+      this.$axios.$get('http://localhost:8080/api/posts').then(response => {
+          response.data.forEach(data => {
+              this.list.push(data.title);
+          });
+          console.log(response);
+      });
+    },
+  data(){
+      return {
+          list: []
+      }
+  },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
