03189cd6236119e691aa0a55c281834553c6ebad
Kaspar <kasparsu@gmail.com>
2019-11-05 14:28:09 +0200

    add posts id

---
+++ components/Card.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{post.title}}
+      </p>
+    </header>
+    <div class="card-image" v-if="post.images.length">
+      <figure class="image is-4by3">
+        <img :src="post.images[0].path" alt="Placeholder image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">{{post.author.name}}</p>
+        </div>
+      </div>
+
+      <div class="content">
+        <p v-html="post.snippet + '...'"></p>
+        <a class="button is-primary">Read more</a>
+        <br>
+        <time datetime="2016-1-1">{{post.human_date}}</time>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ['post'],
+        name: "Card",
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,46 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(el, key) in list" :key="key">{{el}}</li>
-    </ul>
+    <div class="section">
+      <a class="button is-dark" @click="loadPrevPosts" v-if="pagination.prev_page_url">Prev</a>
+      <a class="button is-dark" @click="loadNextPosts" v-if="pagination.next_page_url">Next</a>
+      <div class="columns is-multiline">
+        <div class="column is-one-quarter" v-for="(post,index) in posts" :key="index">
+          <card :post="post"></card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
+import Card from "../components/Card";
 export default {
+    components: {Card},
     mounted(){
-      this.$axios.$get('http://localhost:8080/api/posts').then(response => {
-          response.data.forEach(data => {
-              this.list.push(data.title);
-          });
-          console.log(response);
-      });
+        this.loadPosts('http://localhost:8080/api/posts');
     },
   data(){
       return {
-          list: []
+          posts: [],
+          pagination: {}
       }
   },
     methods:{
-
+        loadPosts(url){
+            this.$axios.$get(url).then(response => {
+                this.posts = response.data;
+                delete response.data;
+                this.pagination = response;
+                console.log(response);
+            });
+        },
+        loadNextPosts(){
+            this.loadPosts(this.pagination.next_page_url);
+        },
+        loadPrevPosts(){
+            this.loadPosts(this.pagination.prev_page_url);
+        }
     }
 }
 </script>
